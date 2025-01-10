ea51e9ebc9a75d102c2d98a2c8d14d539e8e660c
VPavlov <vladislavih_pavlov@mail.ru>
2025-01-11 02:12:30 +0300

    Изменения

--- src/test/java/DbTest.java
+++ src/test/java/DbTest.java
@@ -17,13 +17,13 @@ import java.sql.SQLException;
  * 2) Проверка что в веб части портала - меню "Песочница"->"Товары" - отображаются действия проделанные в БД
  * 3) Проверка что в API отобрадаются действия проделанные в БД
  */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class DbTest extends BaseTest
 {
     Food food = FoodGenerator.getRandomFood();
     @org.junit.jupiter.api.Test
     @DisplayName("CRUD опреции с товарами через БД")
-    public void aBDTestInsert() throws InterruptedException, SQLException {
+    public void BDTestInsert() throws InterruptedException, SQLException {
 
         System.out.printf("Тестовые параметры: %nНазвание:"+ food.name+"%nТип:"+food.type+"%nЭкзотический:"+food.exotic+" %n");
         BaseTest.DBInsert("INSERT INTO FOOD(FOOD_NAME,FOOD_TYPE,FOOD_EXOTIC) VALUES ('"+food.name+"','VEGETABLE',0)");
@@ -36,7 +36,7 @@ public class DbTest extends BaseTest
     }
     @org.junit.jupiter.api.Test
     @DisplayName("Проверка что в веб части портала - меню \"Песочница\"->\"Товары\" - отображаются действия проделанные в БД")
-    public void cWebTest() throws InterruptedException, SQLException {
+    public void WebTest() throws InterruptedException, SQLException {
         app.getMainPage()
                 .selectPointOfMenu("Песочница")
                 .selectSubMenu("Товары")
@@ -46,7 +46,7 @@ public class DbTest extends BaseTest
     }
     @org.junit.jupiter.api.Test
     @DisplayName("Проверка что в API отобрадаются действия из Web формы меню \"Песочница\"->\"Товары\"")
-    public void bApiTestAssert() throws InterruptedException, SQLException {
+    public void ApiTestAssert() throws InterruptedException, SQLException {
         ApiRequest request = RequestFactory.createRequest("GET","http://localhost:8080/api/food",food);
         Response response = request.sendRequest();
         Allure.addAttachment("Результат", "application/json", response.body().prettyPrint());
