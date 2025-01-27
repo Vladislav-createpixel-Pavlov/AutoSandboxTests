5a65281feac566da8f0187ed72b39d76889e8703
VPavlov <vladislavih_pavlov@mail.ru>
2025-01-27 12:29:59 +0300

    Изменения

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -25,7 +25,6 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
  * 3) Проверка что в БД - отображаются действия проделанные в API
  * 2) Проверка что в веб части портала - меню "Песочница"->"Товары" - отображаются действия проделанные в API
  */
-@ExtendWith(MyExecution.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class APITest extends BaseTest{
     static Food food = FoodGenerator.getRandomFood();
